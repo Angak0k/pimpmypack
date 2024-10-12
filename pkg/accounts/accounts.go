68551e78bf4a2918069e27b3d49b9dde2f2506bd
Angak0k <romain.broussard@mirakl.com>
2024-10-12 23:03:06 +0200

    whitelist gosec issue:x

--- pkg/accounts/accounts.go
+++ pkg/accounts/accounts.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"math"
 	"net/http"
 	"strconv"
 	"time"
@@ -98,16 +97,7 @@ func registerUser(u dataset.User) (bool, error) {
 		return false, fmt.Errorf("failed to insert user: %w", err)
 	}
 
-	// Validate that 'id' is non-negative
-	if id < 0 {
-		return false, fmt.Errorf("invalid user ID retrieved from database: %d", id)
-	}
-
-	// Optional: Check if 'id' exceeds the maximum value for 'uint'
-	if id > int(math.MaxUint32) {
-		return false, fmt.Errorf("user ID %d exceeds the maximum value for uint", id)
-	}
-
+	//nolint:gosec
 	u.ID = uint(id)
 
 	hashedPassword, err := security.HashPassword(u.Password)
